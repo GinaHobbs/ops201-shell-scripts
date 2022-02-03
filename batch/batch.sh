548f7f9cf103e3cba65cc7e7c28e8bba13721c5e
Gina Hobbs <ginamariehobbs@gmail.com>
2022-02-03 03:00:36 +0000

    added batch.sh

---
+++ batch/batch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Script Name: batch.sh
+#Author: Gina Hobbs
+#Date of last revision: 2 February 2022
+#Description of purpose: automate copying work files from desktop 
+#Declaration of variables:
+#Declaration of functions (if used):
+#Main
+
+
+
+#End
