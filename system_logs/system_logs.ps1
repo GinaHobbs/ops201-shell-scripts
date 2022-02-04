f18a59e37263e5a17f12baa860a7679142c821a2
Gina Hobbs <ginamariehobbs@gmail.com>
2022-02-04 19:49:20 +0000

    updated system_logs.ps1 again

--- system_logs/system_logs.ps1
+++ system_logs/system_logs.ps1
@@ -12,4 +12,13 @@ $Events = Get-EventLog -LogName System -After (Get-Date).AddDays(-1) | Out-File 
 #Writes out all the Error events into a file on the desktop names errors.txt
 Get-EventLog -LogName System -EntryType Error | Out-File -FilePath "C:\Users\Gina Hobbs\OneDrive\Desktop\errors.txt"
 
+#Prints out all System log entires with an id of 16
+Get-EventLog -LogName System -InstanceID 16
+
+#Prints out the newest 20 entires in the system eventlog
+Get-EventLog -Logname System -Newest 20
+
+#Prints out the sources of the 500 most recent entries in the system event log
+Get-EventLog -LogName System -Newest 500 | Select-Object -Property *
+
 #End
