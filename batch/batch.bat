113aaa5932787c4dd28128c6cbf91cf657375f9d
Gina Hobbs <ginamariehobbs@gmail.com>
2022-02-04 02:34:15 +0000

    updated batch.bat

---
+++ batch/batch.bat
@@ -0,0 +1,15 @@
+::#Script Name: batch.sh
+::#Author: Gina Hobbs
+::#Date of last revision: 2 February 2022
+::#Description of purpose: automate copying work files from desktop 
+::#Declaration of variables:
+::#Declaration of functions (if used):
+::#Main
+
+@echo off
+
+ROBOCOPY "C:\Users\j.thompson\Desktop\My Project Files" E:\
+
+pause
+
+::#End
