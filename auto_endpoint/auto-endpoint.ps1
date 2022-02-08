2a9f54fc8e128f1a194b1ac262c214de56c63705
Gina Hobbs <ginamariehobbs@gmail.com>
2022-02-08 01:42:56 +0000

    added auto-endpoint.ps1

---
+++ auto_endpoint/auto-endpoint.ps1
@@ -0,0 +1,12 @@
+#Script Name: auto-endpoint.ps1
+#Author: Gina Hobbs
+#Date of last revision: 07 February 2022
+#Description of purpose: to Automate Endpoint Configuration
+#Declaration of variables: PID
+#Declaration of functions (if used):
+#Main
+
+
+
+
+#End
