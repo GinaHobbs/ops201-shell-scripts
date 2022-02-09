e006e6daa3d8301e437f3c6d623b6d14b8bb324a
Gina Hobbs <ginamariehobbs@gmail.com>
2022-02-09 13:37:17 +0000

    updated select_string.ps1

--- select_string/select_string.ps1
+++ select_string/select_string.ps1
@@ -2,12 +2,13 @@
 #Author: Gina Hobbs
 #Date of last revision: 08 February 2022
 #Description of purpose: to search a file for the IPV4 information
-#Declaration of variables: 
-#Declaration of functions (if used):
+#Declaration of variables: ipconfig
+#Declaration of functions (if used): get_IPv4
 #Main
 
 function get_IPv4 {
-    ipconfig /all | Out-File network_report.txt
+    $ipconfig = ipconfig /all
+    $ipconfig | Out-File network_report.txt
     Select-String network_report.txt -Pattern 'IPv4'
     Remove-Item network_report.txt
 }
