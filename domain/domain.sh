dce12da8d181ae674c57bb9b02025b6769e847fe
Gina Hobbs <ginamariehobbs@gmail.com>
2022-02-11 18:35:48 +0000

    updated domain.sh

--- domain/domain.sh
+++ domain/domain.sh
@@ -2,10 +2,16 @@
 #Author: Gina Hobbs
 #Date of last revision: 09 February 2022
 #Description of purpose: 
-#Declaration of variables: 
+#Declaration of variables: domain
 #Declaration of functions (if used): 
 #Main
 
+echo "Please type a domain name."
+read domain
 
+whois ${domain}
+dig ${domain}
+host ${domain}
+nslookup ${domain}
 
 #End
